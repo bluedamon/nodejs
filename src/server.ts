c778fa9de6903537a9bbd8a7a9e3d948085b30c5
Damon <damon@usp.br>
2023-07-24 17:41:22 -0300

    add eslint condig

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import fastify from 'fastify';
 
 const app = fastify();
-app.get('/test', () => {
-    return 'Server ON 💻';
-});
+app.get('/test', () => 'Server ON 💻');
 
-app.listen({port: 3333}).then(() => {console.log('Serves ON 💻')})
+app.listen({ port: 3333 }).then(() => { console.log('Server ON 💻'); });
